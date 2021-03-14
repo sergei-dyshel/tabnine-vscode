ace616162608e5ad46f1cdc54bce9d7a331e7e49
Sergei Dyshel <qyron.private@gmail.com>
2021-03-14 22:50:01 +0200

    My tweaks

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 import * as vscode from "vscode";
 import handleAlpha, { updatePersistedAlphaVersion } from "./alphaInstaller";
 import pollDownloadProgress from "./binary/pollDownloadProgress";
@@ -31,12 +32,78 @@ import { registerStatusBar, setDefaultStatus } from "./statusBar/statusBar";
 import { closeValidator } from "./validator/ValidatorClient";
 import executeStartupActions from "./binary/startupActionsHandler";
 
+let providerDisposers: vscode.Disposable[] = [];
+let lastTrigger: string;
+let channel: vscode.OutputChannel;
+
+function getTrigger() {
+  return vscode.workspace.getConfiguration().get<string>('tabnine.trigger', 'off');
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function onConfigurationChange(_: vscode.ConfigurationChangeEvent) {
+  if (lastTrigger === getTrigger())
+    return;
+  lastTrigger = getTrigger();
+  void vscode.window.showInformationMessage('"tabnine.trigger" changed');
+  void registerAllProviders();
+}
+
+function complementLanguages(languages: string[], all: string[]): string[]
+{
+  const allSet = new Set<string>(all);
+  for (const lang of languages)
+    allSet.delete(lang);
+  return Array.from(allSet);
+}
+
+async function registerAllProviders()
+{
+  channel.appendLine('Unregistering all');
+  for (const disposer of providerDisposers)
+    disposer.dispose()
+  providerDisposers = []
+  const allLanguages = await vscode.languages.getLanguages();
+  if (lastTrigger === 'off') {
+    providerDisposers.push(...registerProvider(allLanguages, false));
+  } else if (lastTrigger === 'on') {
+    providerDisposers.push(...registerProvider(allLanguages, true));
+  } else {
+    const languages = lastTrigger.split(',').sort().map(lang => lang.trim());
+    providerDisposers.push(...registerProvider(languages, true));
+    providerDisposers.push(...registerProvider(
+        complementLanguages(languages, allLanguages), false));
+  }
+}
+
+function registerProvider(languages: string[], trigger: boolean) {
+  const triggers = trigger ? COMPLETION_TRIGGERS : [];
+
+  channel.appendLine(
+      `Registering trigger=${trigger} languages=${languages.join(', ')}`);
+
+  return [
+    vscode.languages.registerCompletionItemProvider(
+        languages, {
+          provideCompletionItems,
+        },
+        ...triggers),
+    vscode.languages.registerHoverProvider(languages, {
+      provideHover,
+    })
+  ]
+}
+
 export function activate(context: vscode.ExtensionContext): Promise<void> {
   initBinary();
   handleSelection(context);
   handleUninstall(() => uponUninstall(context));
 
   registerStatusBar(context);
+  channel = vscode.window.createOutputChannel('tabnine');
+  lastTrigger = getTrigger();
+  vscode.workspace.onDidChangeConfiguration(onConfigurationChange);
+
 
   // Do not await on this function as we do not want VSCode to wait for it to finish
   // before considering TabNine ready to operate.
@@ -61,19 +128,7 @@ async function backgroundInit(context: vscode.ExtensionContext) {
   registerCommands(context);
   pollDownloadProgress();
   void executeStartupActions();
-  vscode.languages.registerCompletionItemProvider(
-    { pattern: "**" },
-    {
-      provideCompletionItems,
-    },
-    ...COMPLETION_TRIGGERS
-  );
-  vscode.languages.registerHoverProvider(
-    { pattern: "**" },
-    {
-      provideHover,
-    }
-  );
+  await registerAllProviders();
 }
 
 export async function deactivate(): Promise<unknown> {
